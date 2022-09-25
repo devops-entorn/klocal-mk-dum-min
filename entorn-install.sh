6a0f96727ef8ec02b7e781f4c10b15686aa5e10c
Oriol Boix Anfosso <orboan@gmail.com>
2022-09-25 19:14:00 +0000

    ssh alias added

--- entorn-install.sh
+++ entorn-install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+file="$HOME/.profile"
+
+if grep -q "UserKnownHostsFile" "$file"; then
+        echo
+        echo "ssh alias already set." # String was found
+        echo
+else
+        echo "alias ssh='ssh -o UserKnownHostsFile=/dev/null'" >> $file
+        echo "ssh alias is now set."
+        echo
+fi
+
+
 cd ./k8s-ilg-local/
 ./install-k8s-entorn.sh
 

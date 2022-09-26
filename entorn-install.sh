859b4d604a4bd7cd6bd6c7c47eeec0e47d600169
Oriol Boix Anfosso <orboan@gmail.com>
2022-09-26 14:15:48 +0000

    sshfs mount script+

--- entorn-install.sh
+++ entorn-install.sh
@@ -4,17 +4,25 @@ file="$HOME/.profile"
 
 if grep -q "UserKnownHostsFile" "$file"; then
         echo
-        echo "ssh alias already set." # String was found
+	echo "ssh alias already set." # String was found
         echo
 else
         echo "alias ssh='ssh -o UserKnownHostsFile=/dev/null'" >> $file
-        echo "alias sshfs='sshfs -o UserKnownHostsFile=/dev/null'" >> $file
-	echo "ssh alias is now set."
+	echo "alias sshfs='sshfs -o UserKnownHostsFile=/dev/null'" >> $file
+        echo "ssh alias is now set."
         echo
 fi
 
+mkdir -p $HOME/.local/bin
+cp ./bin/entorn-mount $HOME/.local/bin
+chmod +x $HOME/.local/bin/entorn-mount
+if [ ! -L "$HOME/.local/bin/entorn_mount" ]; then
+	ln -s $HOME/.local/bin/entorn-mount $HOME/.local/bin/entorn_mount
+fi
+echo "entorn-mount is now available"
 
-cd ./k8s-ilg-local/
+
+cd ./k8s-dummy-local/
 ./install-k8s-entorn.sh
 
 exit 0

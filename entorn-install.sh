62734c16ac86d852654fb64ffe292461ab74864f
Oriol Boix Anfosso <orboan@gmail.com>
2022-09-28 12:12:22 +0000

    scripts improved

--- entorn-install.sh
+++ entorn-install.sh
@@ -14,15 +14,25 @@ else
 fi
 
 mkdir -p $HOME/.local/bin
+
 cp ./bin/entorn-mount $HOME/.local/bin
 chmod +x $HOME/.local/bin/entorn-mount
 if [ ! -L "$HOME/.local/bin/entorn_mount" ]; then
 	ln -s $HOME/.local/bin/entorn-mount $HOME/.local/bin/entorn_mount
 fi
 echo "entorn-mount is now available"
+echo
 
+cp ./bin/entorn-restart $HOME/.local/bin
+chmod +x $HOME/.local/bin/entorn-restart
+if [ ! -L "$HOME/.local/bin/entorn_restart" ]; then
+        ln -s $HOME/.local/bin/entorn-restart $HOME/.local/bin/entorn_restart
+fi
+echo "entorn-restart is now available"
+echo
 
 cd ./k8s-ilg-local/
 ./install-k8s-entorn.sh
 
 exit 0
+
--- entorn-pull.sh
+++ entorn-pull.sh
@@ -4,28 +4,41 @@ ver="0.1.0"
 local_base_name=entorn-io/singleuser
 remote_base_name=pluralcamp/entorn-io
 
+error=false
+
 pull_image() {
-docker pull ${remote_base_name}-$1:$ver
-docker tag ${remote_base_name}-$1:$ver $local_base_name:$1-${ver}
-docker rmi ${remote_base_name}-$1:$ver
+if ! docker pull ${remote_base_name}-$1:$ver 2>/dev/null ; then
+	error=true;
+fi
+docker tag ${remote_base_name}-$1:$ver $local_base_name:$1-${ver} 2>/dev/null
+docker rmi ${remote_base_name}-$1:$ver 2>/dev/null
 }
 
 pull_image dev
 pull_image data
 pull_image ops
 
-docker pull ${remote_base_name}-hub:$ver
-docker tag ${remote_base_name}-hub:$ver entorn-io/hub:${ver}
-docker rmi ${remote_base_name}-hub:$ver
+if ! docker pull ${remote_base_name}-hub:$ver 2>/dev/null ; then
+	error=true
+fi
+docker tag ${remote_base_name}-hub:$ver entorn-io/hub:${ver} 2>/dev/null
+docker rmi ${remote_base_name}-hub:$ver 2>/dev/null
 
-docker pull ${remote_base_name}-dind:$ver
-docker tag ${remote_base_name}-dind:$ver entorn-io/dind:${ver}
-docker rmi ${remote_base_name}-dind:$ver
+if ! docker pull ${remote_base_name}-dind:$ver 2>/dev/null ; then
+	error=true
+fi
+docker tag ${remote_base_name}-dind:$ver entorn-io/dind:${ver} 2>/dev/null 
+docker rmi ${remote_base_name}-dind:$ver 2>/dev/null
 
 docker images | grep entorn | grep "$ver"
 
-echo 
-echo "All entorn images pulled successfully."
+echo
+if [ "$error" == "false" ]; then 
+	echo "All entorn images pulled successfully."
+else
+	echo "Error in pulling one or more images."
+	echo "Please run $0 again."
+fi
 echo
 
 exit 0
